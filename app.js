0003a4dba27c8ec641ece13c239201465283fe68
franciscosvr37 <siglasconexion@gmail.com>
2025-10-01 19:23:11 +0000

    mi commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const sum = (a,b) => {
+    return a + b
+}
+console.log(sum(7,3))
+
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar){
+    let valueInYen = valueInDollar * 156.50
+    return valueInYen
+}
+
+const fromYenToPound = function(valueInYen){
+    let valueInPound = valueInYen * 0.87
+    return valueInPound 
+}
+
+module.exports = {sum, fromEuroToDollar, fromDollarToYen, fromYenToPound}
